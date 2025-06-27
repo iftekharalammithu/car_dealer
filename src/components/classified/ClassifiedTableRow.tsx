71676d06a296126ae44de1fef11607d369656726
IftekharAlamMithu <iftekharalammithu23@gmail.com>
2025-06-27 21:12:39 +0600

    chore: update package dependencies and improve ClassifiedTableRow component
    - Updated "@radix-ui/react-slot" to version 1.2.3 in package.json
    - Downgraded "next" to version 15.3.4 and "next-auth" to version 4.24.11 for compatibility
    - Updated "@types/node" to version 20.19.1 for better type definitions
    - Enhanced ClassifiedTableRow component to display images and additional classified details such as title, price, VRM, and color using utility functions for formatting
    - Added a new Badge component for consistent styling across the application

--- src/components/classified/ClassifiedTableRow.tsx
+++ src/components/classified/ClassifiedTableRow.tsx
@@ -1,11 +1,42 @@
 import { ClassifiedWithImages } from "@/config/types";
 import React from "react";
 import { TableCell, TableRow } from "../ui/table";
+import Image from "next/image";
+import { formatColor, formatPrice } from "@/lib/utils";
 
 const ClassifiedTableRow = (classified: ClassifiedWithImages) => {
   return (
     <TableRow className=" text-muted border-white">
       <TableCell className=" font-medium">{classified.id}</TableCell>
+      <TableCell className=" p-0">
+        <Image
+          src={classified.images[0].src}
+          alt={classified.images[0].alt}
+          width={120}
+          height={80}
+          quality={1}
+          className=" aspect-3/2 object-cover rounded"
+        ></Image>
+      </TableCell>
+      <TableCell className="  hidden md:table-cell">
+        {classified.title}
+      </TableCell>
+      <TableCell className="  hidden md:table-cell">
+        {formatPrice({
+          price: classified.price,
+          currency: classified.currency,
+        })}
+      </TableCell>
+      <TableCell className="  hidden md:table-cell">{classified.vrm}</TableCell>
+      <TableCell className="  hidden md:table-cell">
+        {formatColor(classified.color)}
+      </TableCell>
+      <TableCell className="  hidden md:table-cell">
+        {classified.title}
+      </TableCell>
+      <TableCell className="  hidden md:table-cell">
+        {classified.title}
+      </TableCell>
     </TableRow>
   );
 };
